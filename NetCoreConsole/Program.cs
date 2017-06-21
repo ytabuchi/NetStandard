8f693d0f591e48a1ec232cad3a26c6f5ad0747cc
ytabuchi @ Surface <ytabuchi.xlsoft@gmail.com>
2017-06-21 14:27:49 +0900

    Move sln file path

--- NetCoreConsole/Program.cs
+++ NetCoreConsole/Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/* .NET Core does not support code page 932 a.k.a. Shift_JIS as default.
+ * Please install "System.Text.Encoding.CodePages 4.3.0" from NuGet.
+ */
+using System;
+using System.Xml.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text;
 
 namespace NetCoreConsole
 {
@@ -6,7 +13,20 @@ namespace NetCoreConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //コンソールの入力をUTF8で受け取る？
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            Console.WriteLine("文章を入力してください：");
+            var rowConsoleText = Console.ReadLine();
+
+            var translatedText = DoTranslate(rowConsoleText).Result;
+            Console.WriteLine(translatedText);
+        }
+
+        private static async Task<string> DoTranslate(string rowText)
+        {
+            var translatedText = await NetStandardLibrary.TranslateLib.TranslateTextAsync(rowText);
+            return translatedText;
         }
     }
 }
