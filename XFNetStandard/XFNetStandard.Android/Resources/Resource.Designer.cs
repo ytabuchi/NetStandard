80427a8d7a8619de893159ac6989c9eebb222202
ytabuchi @ Surface <ytabuchi.xlsoft@gmail.com>
2017-06-21 15:52:49 +0900

    change XF Projects from PCL to Net Standard 1.4

--- XFNetStandard/XFNetStandard.Android/Resources/Resource.Designer.cs
+++ XFNetStandard/XFNetStandard.Android/Resources/Resource.Designer.cs
@@ -26,6 +26,7 @@ namespace XFNetStandard.Droid
 		
 		public static void UpdateIdValues()
 		{
+			global::Xamarin.Forms.Platform.Android.Resource.Attribute.actionBarSize = global::XFNetStandard.Droid.Resource.Attribute.actionBarSize;
 		}
 		
 		public partial class Animation
