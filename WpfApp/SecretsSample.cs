8f4d047c617d6c222ec11dc676d4c0f4681759de
ytabuchi @ Surface <ytabuchi.xlsoft@gmail.com>
2017-06-25 15:03:06 +0900

    final

---
+++ WpfApp/SecretsSample.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetStandardLibrary
+{
+    //To use it, change file name to "Secrets.cs" then comment out.
+    //public class Secrets
+    //{
+    //    public static readonly string TranslatorApiKey = "XXXXXXXXXXXX";
+    //}
+}
